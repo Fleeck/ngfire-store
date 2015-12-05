cb3bb787e97b9682bb7f7aa8abe46ea7be767546
Fleeck <zoorned@gmail.com>
2015-12-05 15:10:54 +0200

    Added directives and moved pages into separate files.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 var app = angular.module("myApp", []);
+
+app.directive('aboutUs', function() {
+  return {
+    restrict:"E",
+    templateUrl:'about.html'
+  };
+});
+
+app.directive('contacts', function() {
+  return {
+    restrict:'E',
+    templateUrl:'contacts.html'
+  };
+});
+
+app.directive('mainPage', function() {
+  return {
+    restrict:'E',
+    templateUrl:'main.html'
+  };
+});
