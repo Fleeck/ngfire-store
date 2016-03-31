fa36154b64f5266514e634aecc3af1135e40bea4
Artur Kokhanskyi <akokhans@softserveinc.com>
2016-03-31 11:45:11 +0300

    Fixed livereload bug, now reloads.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,9 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-
+  var serveStatic = require('serve-static');
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
-  var serveStatic = require('serve-static');
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -68,7 +67,7 @@ module.exports = function (grunt) {
       options: {
         port: 4000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
+        hostname: 'localhost',
         livereload: 4002
       },
       livereload: {
