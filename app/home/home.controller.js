f5129732fa750d18b06388a190669009a609b501
Artur Kokhanskyi <akokhans@softserveinc.com>
2016-04-08 13:44:07 +0300

    Added functionality for viewing books from admin panel

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,11 +1,12 @@
 angular.module('bookStore')
-  .controller('HomeCtrl', function($state, Auth, Users) {
+  .controller('HomeCtrl', function($state, Auth, Users, Books) {
     var homeCtrl = this;
     homeCtrl.tab = 1;
     homeCtrl.logout = Auth.logout;
     homeCtrl.isLoggedIn = Auth.isLoggedIn;
     homeCtrl.currentUser = Auth.currentUser;
     homeCtrl.getUserName = Users.getUserName;
+    homeCtrl.books = Books.all;
 
     Auth.$onAuth(function(authData) {
       if (authData) {
@@ -21,41 +22,6 @@ angular.module('bookStore')
         };
 
     };
-
-    homeCtrl.products = [{
-      name: 'Гаррі Поттер і в`язень Азкабану',
-      author: 'Джоан Роулінг',
-      price: 60,
-      pubdate: '3 вересня 2004 р.',
-      cover: 'images/harry3.jpg',
-      quantity: 1
-
-    }, {
-      name: 'Гаррі Поттер і філософський камінь',
-      author: 'Джоан Роулінг',
-      price: 65,
-      pubdate: '5 травня 2002 р.',
-      cover: 'images/harry1.jpg',
-      quantity: 1
-    }, {
-      name: 'Гаррі Поттер і таємна кімната',
-      author: 'Джоан Роулінг',
-      price: 59,
-      pubdate: '16 серпня 2003 р.',
-      cover: 'images/harry2.jpg',
-      quantity: 1
-    }, {
-      name: 'Біблія',
-      author: 'Редакція І.Огієнка',
-      price: 50,
-      pubdate: '11 квітня 1998 р.',
-      cover: 'images/bible.jpg',
-      quantity: 1
-    }];
-    // Функція додавання до кошика
-    homeCtrl.added = function(index) {
-      cartjs.cart.add(homeCtrl.products[index]);
-    }
 
     homeCtrl.isSelected = function(checkTab) {
       return homeCtrl.tab === checkTab;
